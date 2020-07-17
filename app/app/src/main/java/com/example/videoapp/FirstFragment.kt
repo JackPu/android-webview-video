06878fcfd0bdef39af6d992a5169d3420e7f47ba
kakashjack@gmail.com <puhuan@fenbi.com>
2020-07-17 12:02:36 +0800

    feat: add exoplayer

--- app/app/src/main/java/com/example/videoapp/FirstFragment.kt
+++ app/app/src/main/java/com/example/videoapp/FirstFragment.kt
@@ -1,12 +1,22 @@
 package com.example.videoapp
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Player.TimelineChangeReason
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.Timeline
+import com.google.android.exoplayer2.source.hls.HlsManifest
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Log
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,8 +34,40 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        if (context != null) {
+            val player = SimpleExoPlayer.Builder(context!!).build()
+            exoplayer_view.player = player
+            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                context,
+                Util.getUserAgent(context!!, "yourApplicationName")
+            )
+
+            val mp4uri = "https://media.vued.vanthink.cn/CJ7%20-%20Trailer.mp4"
+            val hlsuri = "https://media.wxzxzj.com/the_garden_of_words_trailer_english__1080p.m3u8"
+            val hlsMediaSource =
+                HlsMediaSource.Factory(dataSourceFactory).setAllowChunklessPreparation(true).createMediaSource(Uri.parse(hlsuri))
+//            val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+//                .createMediaSource(Uri.parse(hlsuri))
+            player.prepare(hlsMediaSource)
+            player.addListener(
+                object : Player.EventListener {
+                    override fun onTimelineChanged(
+                        timeline: Timeline, @TimelineChangeReason reason: Int
+                    ) {
+                        val manifest: Any? = player.getCurrentManifest()
+                        if (manifest != null) {
+                            val hlsManifest = manifest as HlsManifest
+                            for (item in hlsManifest.mediaPlaylist.segments) {
+                                Log.d("hls", item.url)
+                            }
+                            // Do something with the manifest.
+                        }
+                    }
+                })
         }
+
+
+
+
     }
 }
